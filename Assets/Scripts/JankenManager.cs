79ddbb191807b4b30faebe14f323a274aca15972
miku8323 <takemasa.wadai@gmail.com>
2022-01-06 01:30:33 +0900

    ジャンケン仮完成

--- Assets/Scripts/JankenManager.cs
+++ Assets/Scripts/JankenManager.cs
@@ -10,12 +10,16 @@ public class JankenManager : MonoBehaviour
     private bool isGame = false;
 
     private float time;
-    private int count = 0;
+    private int myCount = 0;
+    private int enemyCount = 0;
+
+    public Animator myAnimator;
+    public Animator enemyAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,31 +27,47 @@ public class JankenManager : MonoBehaviour
     {
         if(isGame){
             time += Time.deltaTime;
-            Debug.Log(time);
         }
 
         // start
         if (Input.GetKeyDown(KeyCode.Return))
         {
             isGame = true;
-            time = 0.0f;
-            count = 0;
-            //janken();
+            myCount = 0;
+            enemyCount = 0;
+            startJanken();
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             myHand = "グー";
+            myAnimator.SetBool ( "gu", true );
+            myAnimator.SetBool ( "choki", false );
+            myAnimator.SetBool ( "pa", false );
+            myAnimator.SetBool ( "none", false );
         }
         
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             myHand = "チョキ";
+            myAnimator.SetBool ( "gu", false );
+            myAnimator.SetBool ( "choki", true );
+            myAnimator.SetBool ( "pa", false );
+            myAnimator.SetBool ( "none", false );
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             myHand = "パー";
+            myAnimator.SetBool ( "gu", false );
+            myAnimator.SetBool ( "choki", false );
+            myAnimator.SetBool ( "pa", true );
+            myAnimator.SetBool ( "none", false );
+        }
+
+        if(time > 1.8f)
+        {
+            judge();
         }
     }
 
@@ -63,22 +83,122 @@ public class JankenManager : MonoBehaviour
         SceneManager.LoadScene(0);     // シーンindex  
     }
 
+    public void judge()
+    {
+        time = 0.0f;
+        myAnimator.SetBool ( "janken", false );
+        enemyAnimator.SetBool ( "janken", false );
+
+        if(myHand == "グー"){
+            switch(enemyHand)
+            {
+                case "グー":
+                    Debug.Log("あいこ");
+                    break;
+                case "チョキ":
+                    Debug.Log("勝ち");
+                    myCount += 1;
+                    enemyCount = 0;
+                    break;
+                case "パー":
+                    Debug.Log("負け");
+                    myCount = 0;
+                    enemyCount += 1;
+                    break;
+            }
+        } else if(myHand == "チョキ") {
+            switch(enemyHand)
+            {
+                case "グー":
+                    Debug.Log("負け");
+                    myCount = 0;
+                    enemyCount += 1;
+                    break;
+                case "チョキ":
+                    Debug.Log("あいこ");
+                    break;
+                case "パー":
+                    Debug.Log("勝ち");
+                    myCount += 1;
+                    enemyCount = 0;
+                    break;
+            }
+        } else if(myHand == "パー") {
+            switch(enemyHand)
+            {
+                case "グー":
+                    Debug.Log("勝ち");
+                    myCount += 1;
+                    enemyCount = 0;
+                    break;
+                case "チョキ":
+                    Debug.Log("負け");
+                    myCount = 0;
+                    enemyCount += 1;
+                    break;
+                case "パー":
+                    Debug.Log("あいこ");
+                    break;
+            }
+        } else {
+            Debug.Log("遅い");
+            myCount = 0;
+            enemyCount += 1;
+        }
+
+        if(myCount > 2 || enemyCount > 2)
+        {
+            // 終了判定
+        } else {
+            // 続行
+            Invoke("janken", 1.0f);
+        }
+    }
+
+    // ジャンケンゲーム
+    public void startJanken()
+    {
+        janken();
+    }
+
+    // ジャンケン1回分
     public void janken()
     {
-        // アニメーション色々（人、文字）
-            
-        int rnd = Random.Range(1,4);
+        time = 0.0f;
 
+        // アニメーション（人）
+        myHand = "none";
+        myAnimator.SetBool ( "none", true );
+        myAnimator.SetBool ( "gu", false );
+        myAnimator.SetBool ( "choki", false );
+        myAnimator.SetBool ( "pa", false );
+
+        myAnimator.SetBool ( "janken", true );
+        enemyAnimator.SetBool ( "janken", true );
+
+        // アニメーション（文字など）
+
+
+        int rnd = Random.Range(1,4);
         switch(rnd)
         {
             case 1:
                 enemyHand = "グー";
+                enemyAnimator.SetBool ( "gu", true );
+                enemyAnimator.SetBool ( "choki", false );
+                enemyAnimator.SetBool ( "pa", false );
                 break;
             case 2:
                 enemyHand = "チョキ";
+                enemyAnimator.SetBool ( "gu", false );
+                enemyAnimator.SetBool ( "choki", true );
+                enemyAnimator.SetBool ( "pa", false );
                 break;
             case 3:
                 enemyHand = "パー";
+                enemyAnimator.SetBool ( "gu", false );
+                enemyAnimator.SetBool ( "choki", false );
+                enemyAnimator.SetBool ( "pa", true );
                 break;
         }
     }
